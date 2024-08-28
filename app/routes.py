8ebbdf58715f796c96cca06faa841391fc2e1f36
Tristan Guarnieri <guarnit@amazon.com>
2024-08-28 09:41:24 -0700

    Add Jinja template with inheritance

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
-from app import app
+from flask import render_template
+from app import app 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'tpguarnieri'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'I loved that movie'
+        },
+        {
+            'author': {'username': 'Smith'},
+            'body': 'I preferred the Avengers'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
